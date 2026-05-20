d0796d970812a5fd289a3436becf4e1ac6a7ec28
Rhavid Gandra <rhaviddgandra@gmail.com>
2026-05-20 10:38:58 -0300

    tentativa build vercel

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+
 import ramo from './assets/ramo.png';
 import noivo from './assets/noivo.jpg';
 import noiva from './assets/noiva.jpg';
 import recepcao from './assets/recepcao.png';
+
+import brasilFlag from './assets/brasil.jpg';
+import colombiaFlag from './assets/colombia.png';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+
 import 'leaflet/dist/leaflet.css';
+
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
 import L from 'leaflet';
+
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
 import {
   Card,
   CardContent,
@@ -27,18 +38,20 @@ import {
   FormControlLabel,
   Radio,
 } from '@mui/material';
-import WalletStripe from './components/WalletStripe/WalletStripe';
-import brasilFlag from './assets/brasil.jpg';
-import colombiaFlag from './assets/colombia.jpg';
+
 import { Modal } from 'antd';
+
+import WalletStripe from './components/WalletStripe/WalletStripe';
 import MusicPlayer from './components/MusicPlayer/MusicPlayer';
 
 delete L.Icon.Default.prototype._getIconUrl;
+
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: markerIcon2x,
   iconUrl: markerIcon,
   shadowUrl: markerShadow,
 });
+
 function App() {
   const [language, setLanguage] = useState('pt');
   const [selectedItem, setSelectedItem] = useState(null);
@@ -47,44 +60,38 @@ function App() {
 
   const translations = {
     pt: {
-      welcome:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non pulvinar lorem, sit amet lacinia dui. Suspendisse potenti. Donec congue dapibus mi. Fusce tempor ex id luctus varius. Maecenas ac lorem non sapien tincidunt interdum vel eget neque. Praesent tempus nunc gravida odio euismod.',
+      welcome: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       countdown: 'CONTAGEM REGRESSIVA',
       days: 'DIAS',
       hours: 'HORAS',
       minutes: 'MINUTOS',
       seconds: 'SEGUNDOS',
       couple: 'O CASAL',
-      coupleText:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non pulvinar lorem, sit amet lacinia dui. Suspendisse potenti. Donec congue dapibus mi. Fusce tempor ex id luctus varius. Maecenas ac lorem non sapien tincidunt interdum vel eget neque. Praesent tempus nunc gravida odio euismod, quis fermentum ipsum sollicitudin. Donec sed nibh vestibulum, mollis diam a, pretium magna. Donec sit amet fermentum urna. Integer sit amet arcu a justo pretium aliquet. Curabitur facilisis sed lacus ut fringilla. Vestibulum eleifend enim eu justo elementum vestibulum. Donec scelerisque diam nunc, eget iaculis nisi aliquam non.',
+      coupleText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       reception: 'RECEPÇÃO',
-      receptionText:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non pulvinar lorem, sit amet lacinia dui. Suspendisse potenti. Donec congue dapibus mi. Fusce tempor ex id luctus varius. Maecenas ac lorem non sapien tincidunt interdum vel eget neque. Praesent tempus nunc gravida odio euismod, quis fermentum ipsum sollicitudin.',
+      receptionText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       gifts: 'LISTA DE PRESENTES',
       giftButton: 'Presentear',
       weddingPlace:
-        'Local do casamento 💍. Recanto Pampulha — Av. Otacílio Negrão de Lima, 7630 - Pampulha, Belo Horizonte - MG',
+        'Recanto Pampulha — Av. Otacílio Negrão de Lima, 7630 - Pampulha, Belo Horizonte - MG',
       paymentForm: 'Formas de pagamento',
     },
 
     es: {
-      welcome:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non pulvinar lorem, sit amet lacinia dui. Suspendisse potenti. Donec congue dapibus mi. Fusce tempor ex id luctus varius. Maecenas ac lorem non sapien tincidunt interdum vel eget neque. Praesent tempus nunc gravida odio euismod.',
+      welcome: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       countdown: 'CUENTA REGRESIVA',
       days: 'DÍAS',
       hours: 'HORAS',
       minutes: 'MINUTOS',
       seconds: 'SEGUNDOS',
       couple: 'LA PAREJA',
-      coupleText:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non pulvinar lorem, sit amet lacinia dui. Suspendisse potenti. Donec congue dapibus mi. Fusce tempor ex id luctus varius. Maecenas ac lorem non sapien tincidunt interdum vel eget neque. Praesent tempus nunc gravida odio euismod, quis fermentum ipsum sollicitudin. Donec sed nibh vestibulum, mollis diam a, pretium magna. Donec sit amet fermentum urna. Integer sit amet arcu a justo pretium aliquet. Curabitur facilisis sed lacus ut fringilla. Vestibulum eleifend enim eu justo elementum vestibulum. Donec scelerisque diam nunc, eget iaculis nisi aliquam non.',
+      coupleText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       reception: 'RECEPCIÓN',
-      receptionText:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non pulvinar lorem, sit amet lacinia dui. Suspendisse potenti. Donec congue dapibus mi. Fusce tempor ex id luctus varius. Maecenas ac lorem non sapien tincidunt interdum vel eget neque. Praesent tempus nunc gravida odio euismod, quis fermentum ipsum sollicitudin.',
+      receptionText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       gifts: 'LISTA DE REGALOS',
       giftButton: 'Regalar',
       weddingPlace:
-        'Lugar de la boda 💍. Recanto Pampulha — Av. Otacílio Negrão de Lima, 7630 - Pampulha, Belo Horizonte - MG',
+        'Recanto Pampulha — Av. Otacílio Negrão de Lima, 7630 - Pampulha, Belo Horizonte - MG',
       paymentForm: 'Formas de pago',
     },
   };
@@ -95,10 +102,16 @@ function App() {
 
   const calculateTimeLeft = () => {
     const now = new Date();
+
     const difference = targetDate - now;
 
     if (difference <= 0) {
-      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+      return {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      };
     }
 
     return {
@@ -108,15 +121,6 @@ function App() {
       seconds: Math.floor((difference / 1000) % 60),
     };
   };
-
-  const presentear = (presente) => {
-    setSelectedItem(presente);
-    setModalPresente(true);
-  };
-
-  function moneyToStripe(value) {
-    return Math.round(Number(value.replace('R$', '').replace(/\./g, '').replace(',', '.')) * 100);
-  }
 
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
@@ -128,18 +132,15 @@ function App() {
     return () => clearInterval(timer);
   }, []);
 
-  const styles = {
-    slide: {
-      height: '300px',
-      background: '#fff',
-      color: '#333',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      fontSize: '24px',
-      borderRadius: '12px',
-    },
+  function moneyToStripe(value) {
+    return Math.round(Number(value.replace('R$', '').replace(/\./g, '').replace(',', '.')) * 100);
+  }
+
+  const presentear = (presente) => {
+    setSelectedItem(presente);
+    setModalPresente(true);
   };
+
   const presentes = [
     {
       id: 1,
@@ -186,12 +187,25 @@ function App() {
     },
   ];
 
+  const styles = {
+    slide: {
+      height: '300px',
+      background: '#fff',
+      color: '#333',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '24px',
+      borderRadius: '12px',
+    },
+  };
+
   const position = [-19.85497, -43.97319];
 
   return (
     <div className="app">
       <MusicPlayer />
-      {/* TOPO COM BANDEIRAS */}
+
       <div className="language-selector">
         <img
           src={brasilFlag}
@@ -207,51 +221,61 @@ function App() {
           className={language === 'es' ? 'active-flag' : ''}
         />
       </div>
+
       <div className="cover-image" />
+
       <div className="welcome-text">
         <p>{t.welcome}</p>
       </div>
+
       <div className="countdown">
-        <div>
-          <p>{t.countdown}</p>
-        </div>
-        <div></div>
+        <p>{t.countdown}</p>
+
         <div className="row">
           <div className="boxStyle">
             <div className="numberStyle">{timeLeft.days}</div>
-            <div className="labelStyle" S>
-              {t.days}
-            </div>
+            <div className="labelStyle">{t.days}</div>
           </div>
+
           <div className="boxStyle">
             <div className="numberStyle">{timeLeft.hours}</div>
             <div className="labelStyle">{t.hours}</div>
           </div>
+
           <div className="boxStyle">
             <div className="numberStyle">{timeLeft.minutes}</div>
             <div className="labelStyle">{t.minutes}</div>
           </div>
+
           <div className="boxStyle">
             <div className="numberStyle">{timeLeft.seconds}</div>
             <div className="labelStyle">{t.seconds}</div>
           </div>
         </div>
       </div>
+
       <div className="sobre-noivos">
-        <img src={ramo} className="img-ramo" alt="Ramo de Flores" />
+        <img src={ramo} className="img-ramo" alt="Ramo" />
+
         <h1 className="title">{t.couple}</h1>
+
         <div className="noivos">
           <div className="noiva">
-            <img src={noiva} alt="Noiva" className="foto-noivos" /> <p>MORGANNA</p>
+            <img src={noiva} alt="Noiva" className="foto-noivos" />
+            <p>MORGANNA</p>
           </div>
+
           <div className="noivo">
-            <img src={noivo} alt="Noivo" className="foto-noivos" /> <p>RAFAEL</p>
+            <img src={noivo} alt="Noivo" className="foto-noivos" />
+            <p>RAFAEL</p>
           </div>
         </div>
+
         <div className="noivos-text">
           <p>{t.coupleText}</p>
         </div>
       </div>
+
       <div className="carousel">
         <Swiper
           modules={[Navigation, Pagination, Autoplay]}
@@ -260,46 +284,61 @@ function App() {
           navigation
           pagination={{ clickable: true }}
           autoplay={{ delay: 6000 }}
-          loop={true}
+          loop
         >
           <SwiperSlide>
             <div style={styles.slide}>Slide 1</div>
           </SwiperSlide>
+
           <SwiperSlide>
             <div style={styles.slide}>Slide 2</div>
           </SwiperSlide>
+
           <SwiperSlide>
             <div style={styles.slide}>Slide 3</div>
           </SwiperSlide>
         </Swiper>
       </div>
+
       <div className="recepcao">
-        <img src={ramo} className="img-ramo" alt="Ramo de Flores" />
+        <img src={ramo} className="img-ramo" alt="Ramo" />
+
         <h1 className="title">{t.reception}</h1>
-        <img src={recepcao} className="img-recepcao" alt="Local da recepcao" />
+
+        <img src={recepcao} className="img-recepcao" alt="Recepção" />
+
         <div className="recepcao-text">
           <p>{t.receptionText}</p>
         </div>
       </div>
+
       <div className="map">
         <MapContainer
           center={position}
           zoom={15}
-          scrollWheelZoom={true}
-          style={{ height: '300px', width: '90%', borderRadius: '16px' }}
+          scrollWheelZoom
+          style={{
+            height: '300px',
+            width: '90%',
+            borderRadius: '16px',
+          }}
         >
           <TileLayer
             attribution="&copy; OpenStreetMap contributors"
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+
           <Marker position={position}>
             <Popup>{t.weddingPlace}</Popup>
           </Marker>
         </MapContainer>
       </div>
+
       <div className="lista-presentes">
-        <img src={ramo} className="img-ramo" alt="Ramo de Flores" />
+        <img src={ramo} className="img-ramo" alt="Ramo" />
+
         <h1 className="title">{t.gifts}</h1>
+
         <Box
           sx={{
             background: '#f7f7f7',
@@ -313,7 +352,10 @@ function App() {
             <Box
               sx={{
                 display: 'grid',
-                gridTemplateColumns: { xs: '1fr', md: '1fr 1fr 1fr' },
+                gridTemplateColumns: {
+                  xs: '1fr',
+                  md: '1fr 1fr 1fr',
+                },
                 gap: '16px',
               }}
             >
@@ -336,35 +378,30 @@ function App() {
                       height: '180px',
                       objectFit: 'cover',
                       borderRadius: '18px',
-                      display: 'block',
                     }}
                   />
-                  <CardContent sx={{ padding: '5px 0 0 0 !important' }}>
+
+                  <CardContent>
                     <Typography
                       sx={{
                         textAlign: 'center',
                         fontSize: '18px',
-                        color: '#555',
-                        lineHeight: '26px',
-                        minHeight: '80px',
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
                       }}
                     >
                       {presente.nome[language]}
                     </Typography>
+
                     <Typography
                       sx={{
                         textAlign: 'center',
                         fontSize: '24px',
                         fontWeight: 'bold',
-                        color: '#444',
                         marginTop: '10px',
                       }}
                     >
                       {presente.preco}
                     </Typography>
+
                     <Button
                       fullWidth
                       variant="contained"
@@ -373,11 +410,6 @@ function App() {
                         height: '55px',
                         borderRadius: '16px',
                         background: '#b88673',
-                        fontSize: '18px',
-                        fontWeight: 'bold',
-                        textTransform: 'none',
-                        boxShadow: 'none',
-                        '&:hover': { background: '#a97563', boxShadow: 'none' },
                       }}
                       onClick={() => presentear(presente)}
                     >
@@ -394,102 +426,58 @@ function App() {
       {modalPresente && selectedItem && (
         <Modal
           title="Detalhes do Presente"
-          closable={{ 'aria-label': 'Custom Close Button' }}
           open={modalPresente}
-          onOk={() => setModalPresente(false)}
           onCancel={() => setModalPresente(false)}
+          footer={null}
         >
-          <Card
-            key={selectedItem.id}
-            sx={{
-              borderRadius: '20px',
-              border: '1px solid #e3e3e3',
-              boxShadow: 'none',
-              padding: '14px',
-            }}
-          >
-            <Box
-              component="img"
-              src={selectedItem.imagem}
-              alt={selectedItem.nome[language]}
-              sx={{
-                width: '100%',
-                height: '180px',
-                objectFit: 'cover',
-                borderRadius: '18px',
-                display: 'block',
-              }}
-            />
-            <CardContent sx={{ padding: '5px 0 0 0 !important' }}>
-              <Typography
-                sx={{
-                  textAlign: 'center',
-                  fontSize: '14px',
-                  color: '#555',
-                  lineHeight: '26px',
-                  minHeight: '40px',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}
-              >
-                {selectedItem.nome[language]}
-              </Typography>
-              <Typography
-                sx={{
-                  textAlign: 'center',
-                  fontSize: '18px',
-                  fontWeight: 'bold',
-                  color: '#444',
-                  marginTop: '10px',
-                }}
-              >
-                {selectedItem.preco}
-              </Typography>
-              <FormControl
-                sx={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  marginTop: '10px',
-                }}
-              >
-                <FormLabel id={`payment-label`}>{t.paymentForm}</FormLabel>
-                <RadioGroup row aria-labelledby={`payment-label`} name="row-radio-buttons-group">
-                  <FormControlLabel
-                    value="pix"
-                    control={<Radio onChange={() => setPaymentForm('pix')} />}
-                    label="Pix"
-                  />
-                  <FormControlLabel
-                    value="nequi"
-                    control={<Radio onChange={() => setPaymentForm('nequi')} />}
-                    label="Llave Bre-B"
-                  />
-                  <FormControlLabel
-                    value="card"
-                    control={<Radio onChange={() => setPaymentForm('card')} />}
-                    label="Cartão"
-                  />
-                </RadioGroup>
-              </FormControl>
-              {paymentForm === 'card' && (
-                <div className="stripe">
-                  <WalletStripe amount={moneyToStripe(selectedItem.preco)} />
-                </div>
-              )}
-              {paymentForm === 'pix' && (
-                <div className="pix">
-                  <p>Instruções para pagamento via Pix:</p>
-                </div>
-              )}
-              {paymentForm === 'nequi' && (
-                <div className="nequi">
-                  <p>Instruciones para pago con Llave Bre-B:</p>
-                </div>
-              )}
-            </CardContent>
-          </Card>
+          <Typography sx={{ marginBottom: '15px' }}>{selectedItem.nome[language]}</Typography>
+
+          <Typography sx={{ marginBottom: '20px' }}>{selectedItem.preco}</Typography>
+
+          <FormControl>
+            <FormLabel>{t.paymentForm}</FormLabel>
+
+            <RadioGroup row value={paymentForm}>
+              <FormControlLabel
+                value="pix"
+                control={<Radio />}
+                label="Pix"
+                onChange={() => setPaymentForm('pix')}
+              />
+
+              <FormControlLabel
+                value="nequi"
+                control={<Radio />}
+                label="Llave Bre-B"
+                onChange={() => setPaymentForm('nequi')}
+              />
+
+              <FormControlLabel
+                value="card"
+                control={<Radio />}
+                label="Cartão"
+                onChange={() => setPaymentForm('card')}
+              />
+            </RadioGroup>
+          </FormControl>
+
+          {paymentForm === 'card' && (
+            <div className="stripe">
+              <WalletStripe amount={moneyToStripe(selectedItem.preco)} />
+            </div>
+          )}
+
+          {paymentForm === 'pix' && (
+            <div className="pix">
+              <p>Instruções para pagamento via Pix</p>
+            </div>
+          )}
+
+          {paymentForm === 'nequi' && (
+            <div className="nequi">
+              <p>Instruciones para pago con Llave Bre-B</p>
+            </div>
+          )}
         </Modal>
       )}
     </div>
